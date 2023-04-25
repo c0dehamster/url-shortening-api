d99f6dd5f5a1639de1ec19eb2141813866625906
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-25 10:04:34 +0500

    Add markup for the form

---
+++ src/routes/Form.svelte
@@ -0,0 +1,9 @@
+<form action="" class="form">
+	<input
+		type="text"
+		class="form__input"
+		placeholder="Shorten a link here..." />
+
+	<button class="button">Shorten it!</button>
+	<p class="error-message">Please add a link</p>
+</form>
