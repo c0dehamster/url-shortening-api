ef1036d3bfef1fa7c8f26243c89e62518204181d
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-25 10:02:27 +0500

    Add markdown for the list item

---
+++ src/routes/ListItem.svelte
@@ -0,0 +1,8 @@
+<li class="list-item">
+	<div class="links">
+		<p class="initial-link">https://www.frontendmentor.io</p>
+		<p class="short-link">https://relink/k4lKyk</p>
+	</div>
+
+	<button class="button">Copy</button>
+</li>
