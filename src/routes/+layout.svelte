51e9a69ad68a5bb18a310584fc7ce03c87feacbd
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-25 09:39:49 +0500

    Add markup to the header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,14 @@
 
 	import Navigation from "./Navigation.svelte"
 	import Footer from "./Footer.svelte"
+
+	import logo from "../lib/images/logo.svg"
 </script>
 
 <div class="app">
 	<header class="header">
+		<img src={logo} alt="Shortly" class="logo" />
+
 		<Navigation />
 	</header>
 
---
+++ src/routes/Navigation.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<nav class="navigation">
+	<button class="button-icon">
+		<span class="sr-only">Menu</span>
+	</button>
+
+	<div class="nav-contents">
+		<ul class="nav-links">
+			<li class="nav-links__list-item">
+				<a href="#0" class="link">Features</a>
+			</li>
+
+			<li class="nav-links__list-item">
+				<a href="#0" class="link">Pricing</a>
+			</li>
+
+			<li class="nav-links__list-item">
+				<a href="#0" class="link">Resources</a>
+			</li>
+		</ul>
+
+		<div class="my-account">
+			<button class="link">Login</button>
+			<button class="button">Sign Up</button>
+		</div>
+	</div>
+</nav>
