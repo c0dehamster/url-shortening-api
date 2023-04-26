b6cc29834cb8a3b1e11e5d11b998bf022046e35c
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-26 21:25:37 +0500

    Move call to action into a separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,7 @@
 	import Form from "./Form.svelte"
 	import ListItem from "./ListItem.svelte"
 	import Services from "./Services.svelte"
-
-	import backgroundCTAMobile from "../lib/images/bg-boost-mobile.svg"
-	import backgroundCTADesktop from "../lib/images/bg-boost-desktop.svg"
+	import CallToAction from "./CallToAction.svelte"
 </script>
 
 <Hero />
@@ -20,19 +18,12 @@
 </div>
 
 <Services />
-
-<div
-	class="call-to-action"
-	style="--background-mobile: url({backgroundCTAMobile}); --background-desktop: url({backgroundCTADesktop})">
-	<h2 class="call-to-action__heading">Boost your links today</h2>
-	<button class="button">Get Started</button>
-</div>
+<CallToAction />
 
 <style>
 	/* General */
 
-	.form-wrapper,
-	.call-to-action {
+	.form-wrapper {
 		padding-inline: var(--padding-inline-outer);
 	}
 
@@ -52,37 +43,5 @@
 
 	.results {
 		gap: 1.5rem;
-	}
-
-	/* Call to action */
-
-	.call-to-action {
-		width: 100%;
-		min-height: 18.75rem;
-
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		gap: 1rem;
-
-		background-color: var(--color-primary-600);
-		background-image: var(--background-mobile);
-		background-size: contain;
-		background-repeat: no-repeat;
-	}
-
-	.call-to-action__heading {
-		color: var(--color-neutral-100);
-
-		font-size: var(--font-size-700);
-	}
-
-	/* Media query */
-
-	@media screen and (min-width: 40rem) {
-	}
-
-	@media screen and (min-width: 72rem) {
 	}
 </style>
---
+++ src/routes/CallToAction.svelte
@@ -0,0 +1,53 @@
+<script>
+	import backgroundCTAMobile from "../lib/images/bg-boost-mobile.svg"
+	import backgroundCTADesktop from "../lib/images/bg-boost-desktop.svg"
+</script>
+
+<div
+	class="call-to-action"
+	style="--background-mobile: url({backgroundCTAMobile}); --background-desktop: url({backgroundCTADesktop})">
+	<h2 class="call-to-action__heading">Boost your links today</h2>
+	<button class="button">Get Started</button>
+</div>
+
+<style>
+	.call-to-action {
+		padding-inline: var(--padding-inline-outer);
+		width: 100%;
+		min-height: 18.75rem;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+
+		background-color: var(--color-primary-600);
+		background-image: var(--background-mobile);
+		background-size: contain;
+		background-repeat: no-repeat;
+	}
+
+	.call-to-action__heading {
+		color: var(--color-neutral-100);
+
+		font-size: var(--font-size-700);
+	}
+
+	/* Media query */
+
+	@media screen and (min-width: 40rem) {
+		.call-to-action {
+			min-height: 15.5rem;
+
+			gap: 2rem;
+
+			background-image: var(--background-desktop);
+			background-size: cover;
+		}
+
+		.call-to-action__heading {
+			font-size: var(--font-size-800);
+		}
+	}
+</style>
